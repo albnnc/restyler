6d44b8d28fbd83ef7285d87573756398fa97dcab
albnnc <albnnc@gmail.com>
2021-06-02 16:29:03 +0300

    0.21.3
    Fix file name.

---
+++ lib/components/TextArea.tsx
@@ -0,0 +1,35 @@
+import React, { forwardRef, TextareaHTMLAttributes } from 'react';
+import { useThemed } from '../hooks';
+import { FormWidgetProps, StyleProps } from '../models';
+
+export interface TextAreaProps
+  extends Omit<
+      TextareaHTMLAttributes<HTMLTextAreaElement>,
+      keyof FormWidgetProps | keyof StyleProps
+    >,
+    FormWidgetProps<string>,
+    StyleProps {}
+
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ wrap, onChange, ...rest }, ref) => {
+    const ThemedTextArea = useThemed('textarea', {
+      path: 'textArea',
+      style: {
+        border: 'none',
+        background: 'transparent',
+        outline: 'none',
+        padding: 0,
+        margin: 0
+      }
+    });
+    return (
+      <ThemedTextArea
+        ref={ref}
+        onChange={e => {
+          onChange?.(e.target.value);
+        }}
+        {...rest}
+      />
+    );
+  }
+);
