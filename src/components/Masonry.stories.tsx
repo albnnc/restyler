b0d29a73ec9194152d11a7c3f7d30423fdf80a14
albnnc <albnnc@gmail.com>
2021-07-06 13:34:11 +0300

    Update masonry story

--- src/components/Masonry.stories.tsx
+++ src/components/Masonry.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useReducer, useRef } from 'react';
-import { Box, Masonry } from 'src';
+import { Box, Button, Masonry } from 'src';
 import { createBlueprint } from 'storybook/utils';
 
 export default {
@@ -7,34 +7,47 @@ export default {
 };
 
 export const Basics = () => {
+  const [columnsCount, toggle] = useReducer(v => (v === 2 ? 3 : 2), 3);
   const heights = [100, 200, 300, 100, 250, 100, 120, 110];
-  const counts = useRef(heights.map(() => 0)).current;
+  const mountsCounts = useRef(heights.map(() => 0)).current;
+  const rendersCounts = useRef(heights.map(() => 0)).current;
+
   const Component = useCallback(({ index, height }) => {
     const [_, update] = useReducer(v => v + 1, 0);
     useEffect(() => {
-      counts[index]++;
+      rendersCounts[index]++;
+    });
+    useEffect(() => {
+      mountsCounts[index]++;
       update();
     }, []);
     return (
       <Box background="rgba(0, 0, 0, 0.2)" css={{ height: `${height}px` }}>
         <Box>#{index}</Box>
-        <Box>Times mounted: {counts[index]}</Box>
+        <Box>Times mounted: {mountsCounts[index]}</Box>
+        <Box>Times rendered: {rendersCounts[index]}</Box>
       </Box>
     );
   }, []);
+
   return (
-    <Masonry
-      columns={{
-        minWidth: '200px',
-        getProps: () => ({ gap: '5px' })
-      }}
-      gap="5px"
-      css={{ width: '100%', maxWidth: '700px' }}
-    >
-      {heights.map((v, i) => (
-        <Component key={i} index={i} height={v} />
-      ))}
-    </Masonry>
+    <Box direction="column">
+      <Button kind="primary" margin={{ bottom: 'medium' }} onClick={toggle}>
+        Toggle
+      </Button>
+      <Masonry
+        columns={{
+          count: columnsCount,
+          getProps: () => ({ gap: '5px' })
+        }}
+        gap="5px"
+        css={{ width: '600px', maxWidth: '700px' }}
+      >
+        {heights.map((v, i) => (
+          <Component key={i} index={i} height={v} />
+        ))}
+      </Masonry>
+    </Box>
   );
 };
 
